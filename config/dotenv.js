f035f20e41d5e145f23bc82c7c47b9d4a3822bd5
Bill Van Etten <bill@bioteam.net>
2018-09-18 13:38:40 -0400

    added env

---
+++ config/dotenv.js
@@ -0,0 +1,9 @@
+// config/dotenv.js
+module.exports = function(env) {
+  return {
+    clientAllowedKeys: [],
+    // Fail build when there is missing any of clientAllowedKeys environment variables.
+    // By default false.
+    failOnMissingKey: false, 
+  };
+};
--- config/environment.js
+++ config/environment.js
@@ -20,6 +20,10 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+      identityPoolId: process.env.IDENTITY_POOL_ID,
+      lambdaRegion: process.env.LAMBDA_REGION,
+      lambdaApi: process.env.LAMBDA_API,
+      lambdaFunction: process.env.LAMBDA_FUNCTION
     }
   };
 
