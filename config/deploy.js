5782632aac304c02bfa94b09bcdc01f632339e0d
Bill Van Etten <bill@bioteam.net>
2018-09-20 15:43:57 -0400

    added cloudformation

--- config/deploy.js
+++ config/deploy.js
@@ -1,9 +1,10 @@
-/* eslint-env node */
-'use strict';
+/* jshint node: true */
 
 module.exports = function(deployTarget) {
-  let ENV = {
-    build: {},
+  var ENV = {
+    build: {
+      environment: "production"
+    },
     pipeline: {
       // This setting runs the ember-cli-deploy activation hooks on every deploy
       // which is necessary in order to run ember-cli-deploy-cloudfront.
@@ -12,30 +13,40 @@ module.exports = function(deployTarget) {
       // `ENV.pipeline.activateOnDeploy = false` to an environment conditional below.
       activateOnDeploy: false
     },
+    "revision-data": {
+      "type": "version-commit"
+    },
+    cloudformation: {
+      accessKeyId: process.env.AWS_KEY,
+      secretAccessKey: process.env.AWS_SECRET,
+      region: 'us-east-1',
+      templateBody: 'file://cfn.yaml',
+      capabilities: ['CAPABILITY_IAM'],
+      parameters: {
+        DomainName: process.env.CFN_DOMAINNAME,
+        CFCertificate: process.env.CFN_CFCERTIFICATE
+      }
+    },
     s3: {
       accessKeyId: process.env.AWS_KEY,
       secretAccessKey: process.env.AWS_SECRET,
+      bucket(context) {
+        return context.cloudformation.outputs.AssetsBucket;
+      },
+      region: 'us-east-1',
       filePattern: '*'
-    },
-    cloudfront: {
-      accessKeyId: process.env.AWS_KEY,
-      secretAccessKey: process.env.AWS_SECRET
     }
   };
 
-  if (deployTarget === 'staging') {
-    ENV.build.environment = 'production';
-    ENV.s3.bucket = process.env.STAGING_BUCKET;
-    ENV.s3.region = process.env.STAGING_REGION;
-    ENV.cloudfront.distribution = process.env.STAGING_DISTRIBUTION;
-  }
+  // if (deployTarget === 'staging') {
+  //   ENV.s3.bucket = process.env.STAGING_BUCKET;
+  //   ENV.s3.region = process.env.STAGING_REGION;
+  // }
 
-  if (deployTarget === 'production') {
-    ENV.build.environment = 'production';
-    ENV.s3.bucket = process.env.PRODUCTION_BUCKET;
-    ENV.s3.region = process.env.PRODUCTION_REGION;
-    ENV.cloudfront.distribution = process.env.PRODUCTION_DISTRIBUTION;
-  }
+  // if (deployTarget === 'production') {
+  //   ENV.s3.bucket = process.env.PRODUCTION_BUCKET;
+  //   ENV.s3.region = process.env.PRODUCTION_REGION;
+  // }
 
   // Note: if you need to build some configuration asynchronously, you can return
   // a promise that resolves with the ENV object instead of returning the
